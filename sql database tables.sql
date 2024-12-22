b65e8e13c2f012f24343eb5986feb6fab0318eec
Shubham Shinde <shuhammks22@gmail.com>
2024-12-22 19:13:19 +0530

    all are done with sql quries

---
+++ sql database tables.sql
@@ -0,0 +1,4 @@
+create database project;
+use project;
+create table s (id int primary key auto_increment,name varchar(500), option_a varchar(100),option_b varchar(100),option_c varchar(100),option_d varchar(100) );
+create table user (id int primary key auto_increment,name varchar(50),email varchar(50),pass varchar(100));
